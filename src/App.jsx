11260a019d290a58d65c1df11da72af46582ee44
shelooks16 <blauyedz@zoho.eu>
2022-11-07 16:23:44 +0000

    added TicTacToe game title and included Roboto font

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,9 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -75,7 +78,13 @@ function App() {
         Start new game
       </button>
 
-      <h2>Current game history</h2>
+      <h2
+        style={{
+          fontWeight: 'normal',
+        }}
+      >
+        Current game history
+      </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
